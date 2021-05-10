16480271b587cafd50c1286cb244a0ec037c745c
Denys Tsybko <72654542+edexmusic@users.noreply.github.com>
2021-05-10 15:07:52 +0300

    Add files via upload

---
+++ UnitTest1Lab2.1/UnitTest1Lab2.1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab2.1/intRange.cpp"
+#include "../lab2.1/intRange.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1Lab21
+{
+	TEST_CLASS(UnitTest1Lab21)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			intRange TEST(0, 4);
+			Assert::AreEqual(TEST.rangeCheck(TEST, 3), true);
+		}
+	};
+}
